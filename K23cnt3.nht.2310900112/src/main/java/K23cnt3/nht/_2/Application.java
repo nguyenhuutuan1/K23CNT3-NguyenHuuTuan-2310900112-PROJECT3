881cd92bd59c5047228e0e36028c038665260d87
nguyenhuutuan1 <https://github.com/nguyenhuutuan1/nguyenhuutuan-k23cnt3-nhom2.git>
2025-12-13 19:37:32 +0700

    update

--- K23cnt3.nht.2310900112/src/main/java/K23cnt3/nht/_2/Application.java
+++ K23cnt3.nht.2310900112/src/main/java/K23cnt3/nht/_2/Application.java
@@ -1,25 +1,16 @@
-package K23cnt3.nht._2; // Thay đổi package này theo dự án của bạn
+package K23cnt3.nht._2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.taphoa.entity") // Quét các entity
-@EnableJpaRepositories(basePackages = "com.taphoa.repository") // Quét các repository
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("=========================================");
-        System.out.println("🚀 ỨNG DỤNG BÁN TẠP HÓA ĐÃ KHỞI ĐỘNG");
-        System.out.println("=========================================");
-        System.out.println("📊 Truy cập các đường dẫn sau:");
-        System.out.println("👉  Frontend: http://localhost:8080");
-        System.out.println("👉  Admin:    http://localhost:8080/admin");
-        System.out.println("👉  H2 Console (nếu enabled): http://localhost:8080/h2-console");
-        System.out.println("👉  API Docs: http://localhost:8080/swagger-ui.html");
-        System.out.println("=========================================");
+        System.out.println("==========================================");
+        System.out.println("ỨNG DỤNG TẠP HÓA NHT ĐANG CHẠY!");
+        System.out.println("Nguyễn Hữu Tuấn - 2310900112");
+        System.out.println("API: http://localhost:8080/api/hello");
+        System.out.println("==========================================");
     }
 }
